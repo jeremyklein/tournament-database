990e7819e4e85a32d63f9f8564c78435b7d8bb85
Jeremy Klein <kleinjj@lafayette.edu>
2015-03-29 22:14:30 -0400

    Added comments to code

--- tournament.py
+++ tournament.py
@@ -17,24 +17,24 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    conn=connect()
-    cursor=conn.cursor()
-    cursor.execute("""DELETE FROM Match""")
-    conn.commit()
+    conn=connect() # creates connection
+    cursor=conn.cursor() # creates cursor
+    cursor.execute("""DELETE FROM Match""") # deletes all records from match
+    conn.commit() # commits changes
 
 def deletePlayers():
     """Remove all the player records from the database."""
     connection=connect() # calls the connect method which returns a db connection
     cursor=connection.cursor()# calls the cursor method from pscopg2
     cursor.execute("DELETE FROM Player;")# inserts name into player table
-    connection.commit()
+    connection.commit() # commits changes
 
 def countPlayers():
     """Returns the number of players currently registered."""
     connection=connect() # calls the connect method which returns a db connection
     cursor=connection.cursor()# calls the cursor method from pscopg2
     cursor.execute("select count(fullname) from player;")# inserts name into player table
-    return(cursor.fetchall()[0][0])
+    return(cursor.fetchall()[0][0]) # returns the value of the only row in the query
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
@@ -47,25 +47,10 @@ def registerPlayer(name):
     """
     connection=connect() # calls the connect method which returns a db connection
     cursor=connection.cursor()# calls the cursor method from pscopg2
-    cmd=("INSERT INTO Player(FullName) VALUES(%s);")
-    cursor.execute(cmd,(name,))# inserts name into player table
-    connection.commit()
+    cmd=("INSERT INTO Player(FullName) VALUES(%s);")#query string
+    cursor.execute(cmd,(name,))# inserts name into player table using parameterized query
+    connection.commit() # commits changes
 
-def createRound():
-
-    conn=connect()
-    cursor=conn.cursor()
-    cursor.execute("""SELECT MAX(RoundNum) FROM Round """)
-    roundNum=cursor.fetchall()[0][0]
-    if (roundNum==None):
-        roundNum=1
-    else:
-        roundNum=roundNum+1
-    cursor.execute("""INSERT INTO Round(RoundNum=%s) VALUES(%s); """,(roundNum,))
-    cursor.execute("""SELECT MAX(RoundId) from Round""")
-    roundid=cursor.fetchall()[0][0]
-    
-    conn.commit()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -81,15 +66,15 @@ def playerStandings():
         matches: the number of matches the player has played
     """
 
-    standings=[]
-    conn=connect()
-    cursor=conn.cursor()
-    cursor.execute(""" SELECT p.PlayerId, p.fullname, a.wins, count(m.matchid)
+    standings=[]# empty array for standing
+    conn=connect()# creates connectin
+    cursor=conn.cursor() #creates cursor
+    cursor.execute(""" SELECT p.PlayerId, p.fullname, a.wins, count(m.matchid) -- returns player, name, wins, and match count
         FROM
-            Player p
-            left join match m on p.PlayerId=m.PlayerId
+            Player p --player table containts playerid and player name
+            left join match m on p.PlayerId=m.PlayerId -- match table is related to player table with player id
             left join 
-                    (SELECT
+                    (SELECT -- this query returns the number of wins a player has
                         COUNT(o.outcome) as wins, m.PlayerId
                         FROM Match m 
                         JOIN Outcome o
@@ -99,16 +84,15 @@ def playerStandings():
                         ) a
             on a.PlayerId=p.PlayerId
 
-        GROUP BY p.PlayerId, p.fullname, a.wins, m.matchid
+        GROUP BY p.PlayerId, p.fullname, a.wins, m.matchid -- because the select statement includes an aggregation (count) values must be grouped
 
         """)
-    results=cursor.fetchall()
-    for i in range(0, countPlayers()):
-        standings.append((results[i][0],
+    results=cursor.fetchall() # results are stored
+    for i in range(0, countPlayers()): # for all players
+        standings.append((results[i][0], # add tuple to array
             results[i][1],
-            int((results[i][2]) or 0),
+            int((results[i][2]) or 0), 
             int(results[i][3]),))
-    print standings
     return standings    
 
 
@@ -119,18 +103,18 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    conn=connect()
-    cursor=conn.cursor()
-    cursor.execute("SELECT max(matchid) from match")
-    maxid=(cursor.fetchall()[0][0] or 0)+1
-    cmd=("INSERT INTO Match(Outcomeid,Matchid,Playerid) VALUES(%s,%s,%s)")
-    cursor.execute("SELECT Outcomeid from Outcome where Outcome='win'")
-    winid=cursor.fetchall()[0][0]
-    cursor.execute("SELECT Outcomeid from Outcome where Outcome='loss'")
-    lossid=cursor.fetchall()[0][0]
-    cursor.execute(cmd,(winid,maxid,winner))
-    cursor.execute(cmd,(lossid,maxid,loser))
-    conn.commit()
+    conn=connect()# create connection
+    cursor=conn.cursor() # create cursor
+    cursor.execute("SELECT max(matchid) from match") # Get the latest match number
+    maxid=(cursor.fetchall()[0][0] or 0)+1 # create a new match id
+    cmd=("INSERT INTO Match(Outcomeid,Matchid,Playerid) VALUES(%s,%s,%s)") # string template to insert new values
+    cursor.execute("SELECT Outcomeid from Outcome where Outcome='win'") #get the id for outcome
+    winid=cursor.fetchall()[0][0] # return only value of this query
+    cursor.execute("SELECT Outcomeid from Outcome where Outcome='loss'") # get the id for outcome
+    lossid=cursor.fetchall()[0][0] #return only value of this query
+    cursor.execute(cmd,(winid,maxid,winner)) # insert values into database for winner
+    cursor.execute(cmd,(lossid,maxid,loser)) # insert values into database for loser
+    conn.commit()# commit connection to save values
 
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
@@ -147,10 +131,10 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    swissPairings=[]
-    conn=connect()
-    cursor=conn.cursor()
-    cursor.execute(""" SELECT p.PlayerId, p.fullname, sum(o.value)
+    swissPairings=[] # create empty array
+    conn=connect()# create connection to database
+    cursor=conn.cursor() # cursor for connection
+    cursor.execute(""" SELECT p.PlayerId, p.fullname, sum(o.value) -- return payer id full name and the sum of outcome
         FROM
             Player p
             left join match m on p.PlayerId=m.PlayerId
@@ -159,22 +143,15 @@ def swissPairings():
         GROUP BY p.PlayerId, p.fullname
         ORDER BY sum(o.value) 
         """)
-    results=cursor.fetchall()
-    for i in range(0, countPlayers(),2):
-        swissPairings.append((results[i][0],results[i][1],results[i+1][0],results[i+1][1],))
+    results=cursor.fetchall() # save results
+    for i in range(0, countPlayers(),2): # iterate through the results
+        swissPairings.append((results[i][0],results[i][1],results[i+1][0],results[i+1][1],)) # pair the players in order of the list
     return swissPairings    
 
-def createMatch(PlayerOne,PlayerTwo, Round):
-    """
-    Used to create a match in the database between two players
-    """
-    conn=conenct()
-    cursor=connn.cursor()
-    cursor.execute("""
-        INSERT into
-        """)
 
 def addPlayers(num_Players):
+    """Helper method for personal testing to add an arbitrary number of new players to the database
+    """
     for i in range(num_Players):
         name=raw_input("Enter Player name: ")
         registerPlayer(name)
